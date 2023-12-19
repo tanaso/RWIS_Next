23c0c9abdb53189c2ccdbfbf213bbe3e83dd668b
Laure_htr <laure.h67@orange.fr>
2023-12-19 17:58:26 +0900

    Encyclopedia

--- pages/encyclopedia.tsx
+++ pages/encyclopedia.tsx
@@ -2,186 +2,155 @@ import Page from '@/components/page'
 import Section from '@/components/section'
 
 import React from 'react';
-// import EncyclopediaEntry from './EncyclopediaEntry';
-// import encyclopediaData from './data';
-
-// Encyclopedia.js
 
 
 
-
-
-
-// data.js
-
-/*
-export const encyclopediaData = [
-	{
-	  id: 1,
-	  title: 'Black Iris',
-	  description: 'Description du sujet 1...',
-	  imageUrl: 'lien_vers_image1.jpg',
-	},
-	{
-	  id: 2,
-	  title: 'Blue Iris',
-	  description: 'Description du sujet 2...',
-	  imageUrl: 'lien_vers_image2.jpg',
-	},
-	{
-	  id: 3,
-	  title: 'Orange Iris',
-	  description: 'Description du sujet 3...',
-	  imageUrl: 'lien_vers_image3.jpg',
-	},
-	{
-	  id: 4,
-	  title: 'Pink Iris',
-	  description: 'Description du sujet 4...',
-	  imageUrl: 'lien_vers_image4.jpg',
-	},
-	{
-	  id: 5,
-	  title: 'Purple Iris',
-	  description: 'Description du sujet 5...',
-	  imageUrl: 'lien_vers_image5.jpg',
-	},
-	{
-	  id: 6,
-	  title: 'Red Iris',
-	  description: 'Description du sujet 6...',
-	  imageUrl: 'lien_vers_image6.jpg',
-	},
-	{
-	  id: 7,
-	  title: 'White Iris',
-	  description: 'Description du sujet 7...',
-	  imageUrl: 'lien_vers_image7.jpg',
-	},
-	{
-	  id: 8,
-	  title: 'Yellow Iris',
-	  description: 'Description du sujet 8...',
-	  imageUrl: 'lien_vers_image8.jpg',
-	},
-  ];
-
-
-  const Encyclopedia = () => (
-	<Page>
-	  <Section>
-		<h2 className='text-xl font-semibold'>Encyclopedia</h2>
-		<div className='mt-2'>
-		  //{/ ... Votre contenu ici ... /}
-		</div>
-	  </Section>
-  
-	  <div>
-		<h1>Encyclopedia</h1>
-		{encyclopediaData.map(entry => (
-		  <EncyclopediaEntry
-			key={entry.id}
-			title={entry.title}
-			description={entry.description}
-			imageUrl={entry.imageUrl}
-		  />
-		))}
-	  </div>
-	</Page>
-	
-  );
-  
-  export { Encyclopedia }; // Exportations nommées
-
-export const EncyclopediaEntry: React.FC<{ title: string; description: string; imageUrl: string }> = ({ title, description, imageUrl }) => {
-	return (
-	  <div>
-		<h2>{title}</h2>
-		<img src={imageUrl} alt={title} style={{ maxWidth: '100%' }} />
-		<p>{description}</p>
-	  </div>
-	);
-  };
-  
-
-
-// App.js
-
-const App = () => {
-  return (
-    <div>
-      <h1>Mon Application React</h1>
-      <Encyclopedia />
-    </div>
-  );
-};
-
-export default App;
-
-*/
-
-
-
-
-// Flower data with name and image URL
+// Flower data with name, image URL, and locked status
 const flowerData = [
-  { id: 1, name: 'Black Iris', imageUrl: 'Black.png' },
-  { id: 2, name: 'Blue Iris', imageUrl: 'Blue.png' },
-  { id: 3, name: 'Orange Iris', imageUrl: 'Orange.png' },
-  { id: 4, name: 'Pink Iris', imageUrl: 'Pink.png' },
-  { id: 5, name: 'Purple Iris', imageUrl: 'Purple.png' },
-  { id: 6, name: 'Red Iris', imageUrl: 'Red.png' },
-  { id: 7, name: 'White Iris', imageUrl: 'White.png' },
-  { id: 8, name: 'Yellow Iris', imageUrl: 'Yellow.png' },
-  
-  // Add more flowers as needed
+  { id: 1, name: 'Black Iris', imageUrl: './images/Black.png', locked: false, description: "This flower is a symbole of the elegance of your task management" },
+  { id: 2, name: 'Blue Iris', imageUrl: './images/Blue.png', locked: false, description: "Let's be honnest, doing tasks is funnier with a garden" },
+  { id: 3, name: 'Orange Iris', imageUrl: './images/Orange.png', locked: false, description: "Your ambition for doing task is growing ! But before climbing a mountain, let's start with a task" },
+  { id: 4, name: 'Pink Iris', imageUrl: './images/Pink.png', locked: false, description: "Nothing is better than a coffe break after doing a task" },
+  { id: 5, name: 'Purple Iris', imageUrl: './images/Purple.png', locked: false, description: "Add a little bit of magic and mystery by randomly picking your next task ! Garden, Garden on my phone, what will be my next task ?" },
+  { id: 6, name: 'Red Iris', imageUrl: './images/Red.png', locked: false, description: "This flower represents your growing energy to complete tasks. Keep going !" },
+  { id: 7, name: 'White Iris', imageUrl: './images/White.png', locked: false, description: "If perfection was real, it would be the way you manage your tasks" },
+  { id: 8, name: 'Yellow Iris', imageUrl: './images/Yellow.png', locked: false, description: "The more tasks you complete, the less you have to do ! Keep that in mind" },
 ];
-
-
 
 // Define FlowerProps interface for the props
 interface FlowerProps {
   name: string;
   imageUrl: string;
+  locked: boolean;
+  description : string;
 }
 
-// Flower component with TypeScript
-const Flower: React.FC<FlowerProps> = ({ name, imageUrl }: FlowerProps) => (
-  <div>
-    <h3>{name}</h3>
-    <img src={imageUrl} alt={name} style={{ maxWidth: '100%', height: 'auto' }} />
-  </div>
-);
 
-export default Flower;
+/*
+// Flower component with TypeScript
+const Flower: React.FC<FlowerProps> = ({ name, imageUrl, locked }: FlowerProps) => {
+	// Choisissez l'image en fonction du statut (bloqué ou débloqué)
+	const imageSource = locked ? './images/Locked.png' : imageUrl;
+	
+	// Choisissez le texte du nom en fonction du statut (bloqué ou débloqué)
+	const flowerName = locked ? 'Locked' : name;
+  
+	return (
+	  <div style={{ 
+		display: 'flex',
+		alignItems: 'center',
+		border: '1px solid #ccc',
+		padding: '10px',
+		borderRadius: '5px',
+		marginBottom: '10px',
+		opacity: locked ? 0.5 : 1,
+		pointerEvents: locked ? 'none' : 'auto',
+	  }}>
+		<img
+		  src={imageSource}
+		  alt={name}
+		  style={{ maxWidth: '100%', height: 'auto', marginRight: '10px', maxHeight: '100px' }}
+		/>
+		<h3 style={{ fontSize: '1.5em' }}>{flowerName}</h3>
+	  </div>
+	);
+  };
+  
+  
+
+  // Flower component with TypeScript
+const Flower: React.FC<FlowerProps> = ({ name, imageUrl, locked }: FlowerProps) => {
+	// Choisissez l'image en fonction du statut (bloqué ou débloqué)
+	const imageSource = locked ? './images/Locked.png' : imageUrl;
+  
+	// Choisissez le texte du nom en fonction du statut (bloqué ou débloqué)
+	const flowerName = locked ? 'Locked' : name;
+  
+	// Styles pour les fleurs débloquées
+	const unlockedStyles = {
+	  border: '1px solid #f0e6d2', // Couleur du cadre beige très clair
+	  backgroundColor: '#f9f5e7', // Couleur de fond beige très clair
+	  color: 'black', // Couleur du texte en noir
+	};
+  
+	return (
+	  <div style={{
+		display: 'flex',
+		alignItems: 'center',
+		padding: '10px',
+		borderRadius: '5px',
+		marginBottom: '10px',
+		opacity: locked ? 0.5 : 1,
+		pointerEvents: locked ? 'none' : 'auto',
+		...(!locked && unlockedStyles), // Appliquer les styles pour les fleurs débloquées
+	  }}>
+		<img
+		  src={imageSource}
+		  alt={name}
+		  style={{ maxWidth: '100%', height: 'auto', marginRight: '10px', maxHeight: '100px' }}
+		/>
+		<h3 style={{ fontSize: '1.5em', ...(locked ? {} : unlockedStyles) }}>{flowerName}</h3>
+	  </div>
+	);
+  };
+
+  */
+
+// Flower component with TypeScript
+const Flower: React.FC<FlowerProps> = ({ name, imageUrl, locked, description }: FlowerProps) => {
+	// Choisissez l'image en fonction du statut (bloqué ou débloqué)
+	const imageSource = locked ? './images/Locked.png' : imageUrl;
+  
+	// Choisissez le texte du nom en fonction du statut (bloqué ou débloqué)
+	const flowerName = locked ? 'Locked' : name;
+  
+	// Styles pour les fleurs débloquées
+	const unlockedStyles = {
+	  border: '1px solid #f0e6d2', // Couleur du cadre beige très clair
+	  backgroundColor: '#f9f5e7', // Couleur de fond beige très clair
+	  color: 'black', // Couleur du texte en noir
+	};
+  
+	return (
+	  <div style={{
+		display: 'flex',
+		alignItems: 'center',
+		padding: '10px',
+		borderRadius: '5px',
+		marginBottom: '10px',
+		opacity: locked ? 0.5 : 1,
+		pointerEvents: locked ? 'none' : 'auto',
+		...(!locked && unlockedStyles), // Appliquer les styles pour les fleurs débloquées
+	  }}>
+		<img
+		  src={imageSource}
+		  alt={name}
+		  style={{ maxWidth: '100%', height: 'auto', marginRight: '10px', maxHeight: '100px' }}
+		/>
+		<div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+		  <h3 style={{ fontSize: '1.5em', ...(locked ? {} : unlockedStyles) }}>{flowerName}</h3>
+		  {description && <p style={{ margin: '0' }}>{description}</p>}
+		</div>
+	  </div>
+	);
+  };
+  
+  
+  
+  
 
 // Encyclopedia component to display a list of flowers
 const Encyclopedia = () => (
-  <div>
-    <h1>Flower Encyclopedia</h1>
-    {flowerData.map(flower => (
-      <Flower key={flower.id} name={flower.name} imageUrl={flower.imageUrl} />
-    ))}
-  </div>
+  <Page>
+    <Section>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ fontSize: '2em', marginBottom: '50px' }}>Encyclopedia</h1>
+        {flowerData.map(flower => (
+          <Flower key={flower.id} name={flower.name} imageUrl={flower.imageUrl} locked={flower.locked} description={flower.description}  />
+        ))}
+      </div>
+    </Section>
+  </Page>
 );
 
-export { Encyclopedia };
-
-
-const Encyclopedia2 = () => (
-	<Page>
-	  <Section>
-		<h2 className='text-xl font-semibold'>Encyclopedia</h2>
-		<div className='mt-2'>
-
-		</div>
-	  </Section>
-  
-	  <div>
-		<h1>Encyclopedia</h1>
-		
-		 
-	  </div>
-	</Page>
-	
-  );
+export default Encyclopedia;
