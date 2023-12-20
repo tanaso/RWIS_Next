aebf506caf6ef0d1f1155752491f50df12c960ad
Laure_htr <laure.h67@orange.fr>
2023-12-20 18:51:19 +0900

    Spelling mistakes

--- pages/encyclopedia.tsx
+++ pages/encyclopedia.tsx
@@ -7,9 +7,9 @@ import React from 'react';
 
 // Flower data with name, image URL, and locked status
 const flowerData = [
-  { id: 1, name: 'Black Iris', imageUrl: './images/Black.png', locked: false, description: "This flower is a symbole of the elegance of your task management" },
-  { id: 2, name: 'Blue Iris', imageUrl: './images/Blue.png', locked: false, description: "Let's be honnest, doing tasks is funnier with a garden" },
-  { id: 3, name: 'Orange Iris', imageUrl: './images/Orange.png', locked: false, description: "Your ambition for doing task is growing ! But before climbing a mountain, let's start with a task" },
+  { id: 1, name: 'Black Iris', imageUrl: './images/Black.png', locked: false, description: "This flower is a symbol of the elegance of your task management" },
+  { id: 2, name: 'Blue Iris', imageUrl: './images/Blue.png', locked: false, description: "Let's be honest, doing tasks is funnier with a garden" },
+  { id: 3, name: 'Orange Iris', imageUrl: './images/Orange.png', locked: false, description: "Your ambition for doing tasks is growing ! But before climbing a mountain, let's start with a task" },
   { id: 4, name: 'Pink Iris', imageUrl: './images/Pink.png', locked: false, description: "Nothing is better than a coffe break after doing a task" },
   { id: 5, name: 'Purple Iris', imageUrl: './images/Purple.png', locked: false, description: "Add a little bit of magic and mystery by randomly picking your next task ! Garden, Garden on my phone, what will be my next task ?" },
   { id: 6, name: 'Red Iris', imageUrl: './images/Red.png', locked: false, description: "This flower represents your growing energy to complete tasks. Keep going !" },
@@ -26,75 +26,7 @@ interface FlowerProps {
 }
 
 
-/*
-// Flower component with TypeScript
-const Flower: React.FC<FlowerProps> = ({ name, imageUrl, locked }: FlowerProps) => {
-	// Choisissez l'image en fonction du statut (bloqué ou débloqué)
-	const imageSource = locked ? './images/Locked.png' : imageUrl;
-	
-	// Choisissez le texte du nom en fonction du statut (bloqué ou débloqué)
-	const flowerName = locked ? 'Locked' : name;
-  
-	return (
-	  <div style={{ 
-		display: 'flex',
-		alignItems: 'center',
-		border: '1px solid #ccc',
-		padding: '10px',
-		borderRadius: '5px',
-		marginBottom: '10px',
-		opacity: locked ? 0.5 : 1,
-		pointerEvents: locked ? 'none' : 'auto',
-	  }}>
-		<img
-		  src={imageSource}
-		  alt={name}
-		  style={{ maxWidth: '100%', height: 'auto', marginRight: '10px', maxHeight: '100px' }}
-		/>
-		<h3 style={{ fontSize: '1.5em' }}>{flowerName}</h3>
-	  </div>
-	);
-  };
-  
-  
 
-  // Flower component with TypeScript
-const Flower: React.FC<FlowerProps> = ({ name, imageUrl, locked }: FlowerProps) => {
-	// Choisissez l'image en fonction du statut (bloqué ou débloqué)
-	const imageSource = locked ? './images/Locked.png' : imageUrl;
-  
-	// Choisissez le texte du nom en fonction du statut (bloqué ou débloqué)
-	const flowerName = locked ? 'Locked' : name;
-  
-	// Styles pour les fleurs débloquées
-	const unlockedStyles = {
-	  border: '1px solid #f0e6d2', // Couleur du cadre beige très clair
-	  backgroundColor: '#f9f5e7', // Couleur de fond beige très clair
-	  color: 'black', // Couleur du texte en noir
-	};
-  
-	return (
-	  <div style={{
-		display: 'flex',
-		alignItems: 'center',
-		padding: '10px',
-		borderRadius: '5px',
-		marginBottom: '10px',
-		opacity: locked ? 0.5 : 1,
-		pointerEvents: locked ? 'none' : 'auto',
-		...(!locked && unlockedStyles), // Appliquer les styles pour les fleurs débloquées
-	  }}>
-		<img
-		  src={imageSource}
-		  alt={name}
-		  style={{ maxWidth: '100%', height: 'auto', marginRight: '10px', maxHeight: '100px' }}
-		/>
-		<h3 style={{ fontSize: '1.5em', ...(locked ? {} : unlockedStyles) }}>{flowerName}</h3>
-	  </div>
-	);
-  };
-
-  */
 
 // Flower component with TypeScript
 const Flower: React.FC<FlowerProps> = ({ name, imageUrl, locked, description }: FlowerProps) => {
