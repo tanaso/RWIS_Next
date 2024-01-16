f27d14e3995347a04f33a0e6fdd38675ca1e988a
Sota Tanaka <tennisboy_s@me.com>
2024-01-16 14:18:50 +0900

    Refactor fab buttons and add background color to new-category page

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,16 +60,18 @@ const Index = () => {
 						</div>
 					))}
 				</div>
-				<Fab aria-label="add" onClick={handleAdd} className={`${styles.fabStyle} ${styles.bottom100}`}>
+				<Fab aria-label="add" onClick={handleAdd} style={{ position: 'fixed', right: '20px', bottom: '100px', border: '1px solid white', background: '#2A6B41', color: '#D0EFDB' }}
+>
+
 					<AddIcon />
 				</Fab>
 				{isButtonClickable && (
 					<>
-						<Fab variant="extended" onClick={handleAddNewTask} color="primary" aria-label="add" className={`${styles.fabStyle} ${styles.bottom170}`}>
+						<Fab variant="extended" onClick={handleAddNewTask} color="primary" aria-label="add" style={{ position: 'fixed', right: '20px', bottom: '170px', border: '1px solid white', background: '#2A6B41', color: '#D0EFDB' }}>
 							<AddIcon />
 							<span className='fabText'>New Task</span>
 						</Fab>
-						<Fab variant="extended" onClick={handleAddNewCategory} color="primary" aria-label="add" className={`${styles.fabStyle} ${styles.bottom240}`}>
+						<Fab variant="extended" onClick={handleAddNewCategory} color="primary" aria-label="add" style={{ position: 'fixed', right: '20px', bottom: '240px', border: '1px solid white', background: '#2A6B41', color: '#D0EFDB' }}>
 							<AddIcon />
 							<span className='fabText'>New Category</span>
 						</Fab>
