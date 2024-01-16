680dbae2078e176e88956788460242f5ec7edff5
davide <davidelaureti54@gmail.com>
2024-01-16 12:32:08 +0900

    TWA

---
+++ app/src/main/java/app/vercel/rwis_next/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.rwis_next.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
