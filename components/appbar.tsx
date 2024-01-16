99f41de60f4d38dd30ce38efa896368ad836c3fc
Laure_htr <laure.h67@orange.fr>
2024-01-16 16:39:59 +0900

    Profile picture

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -40,8 +40,7 @@ const Appbar = () => {
 							title='Gluten Free'
 							className='h-10 w-10 rounded-full bg-zinc-200 bg-cover bg-center shadow-inner dark:bg-zinc-800'
 							style={{
-								backgroundImage:
-									'url(https://images.unsplash.com/photo-1612480797665-c96d261eae09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80)',
+								backgroundImage: 'url(/images/cat.png)',
 							}}
 						/>
 					</nav>
